462402c301b0420c22015cbf3823bd57cf30ead5
Namdev shinde <86095764+Shindenamdev@users.noreply.github.com>
2024-02-20 10:43:37 +0530

    Create removercharacter.java

---
+++ removercharacter.java
@@ -0,0 +1,15 @@
+//removercharacter
+class removercharacter {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        String s="learnjava";
+    //    char[] replace=s.toCharArray();
+        String result;
+        result=s.replace("a","");
+         System.out.println(result);
+    }
+}
+
+///output
+//Hello, World!
+//lernjv
