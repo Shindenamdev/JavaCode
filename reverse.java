93ad0b9f2516e989bf6718e405f0a2bba5d84353
Namdev shinde <86095764+Shindenamdev@users.noreply.github.com>
2024-02-20 10:36:25 +0530

    Create reverse.java

---
+++ reverse.java
@@ -0,0 +1,14 @@
+
+class reverse {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        String s="namdevpatilshinde";
+        char[] ch=s.toCharArray();
+        for(int i=ch.length-1;i>=0;i--){
+             System.out.print(ch[i]);
+        }
+    }
+}
+//output
+//Hello, World!
+//ednihslitapvedman
